07bbd9f23a2f95d831e8a6b06e652b43a80d8617
nand <nandkv.online@gmail.com>
2018-04-03 10:44:04 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# angularspring
+### Multi module setup:
+### Client: Angular2
+### Server: Spring Boot
+
+## Steps to run:
+#### mvn clean install
+#### cd server
+#### mvn spring-boot:run
---
+++ server/src/main/java/com/example/angularspring/angularspring/AngularspringApplication.java
@@ -0,0 +1,12 @@
+package com.example.angularspring.angularspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularspringApplication.class, args);
+	}
+}
